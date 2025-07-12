2423b2e41fc6abf6ba6d0af0d491ace93caf0548
Goldenjet88 <150926875+Goldenjet88@users.noreply.github.com>
2025-07-12 07:36:11 +0000

    ok

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all codes to newer version
+
+
+//alert(3 +3)// we are using node js not browser
+
+console.log(3+3)
+//code readability need to be high//
+console.log("ak")
+
+
+let name = "ak47"
+let age = 18
+let isloggedIn = false
+let state;
+
+
+// number => 2^53
+//bigint for big numbers trading , big data
+//string"" double qoute must!!
+//boolean =>true/false
+// null == > standalone value empty value ... not string ...
+// undefined ==> not assigned value
+//symbol in react uses for defining unique 
+
+
+
+
+//object 
+
+
+
+console.log(typeof undefined);
+
+
+
+ 
