cebd3f789f5e7d21d66af2d1d4d19dfb9657ca80
Goldenjet88 <150926875+Goldenjet88@users.noreply.github.com>
2025-07-13 17:09:21 +0000

    coverstionofdatatypes

---
+++ 01_basic/03_converstionVariables.js
@@ -0,0 +1,22 @@
+
+let k="true"
+//console.log(typeof (k));
+//console.log(typeof (k));
+let valueInNumber = Number (k)
+//console.log(typeof (valueInNumber));
+//console.log( (valueInNumber));
+
+
+//"33"==>>>33\//
+//"33abc"=>>NaN
+//true=>1,false=> 0
+
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+ let N = 33
+ let M = String(N)
+ console.log(M);
+ console.log(typeof M);
+ 
